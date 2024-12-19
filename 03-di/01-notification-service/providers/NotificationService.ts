fa48271b62f524a350b32c9f44f51d476390f569
grid <grid@sictamil.com>
2024-12-19 15:03:34 +0100

    03-di-01-notification-service add tasks basic impl

--- 03-di/01-notification-service/providers/NotificationService.ts
+++ 03-di/01-notification-service/providers/NotificationService.ts
@@ -1,1 +1,12 @@
-export class NotificationService {}
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class NotificationService {
+  sendEmail(to: string, subject: string, message: string): void {
+    console.log(`Email sent to ${to}: [${subject}] ${message}`);
+  }
+
+  sendSMS(to: string, message: string) {
+    console.log(`SMS sent to ${to}: ${message}`);
+  }
+}
--- 03-di/01-notification-service/tasks/tasks.service.ts
+++ 03-di/01-notification-service/tasks/tasks.service.ts
@@ -1,14 +1,22 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto, Task, TaskStatus, UpdateTaskDto } from "./task.model";
+import { NotificationService } from "../providers/NotificationService";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  constructor() {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly notificationService: NotificationService
+  ) {}
 
   async createTask(createTaskDto: CreateTaskDto) {
     const { title, description, assignedTo } = createTaskDto;
+
+    const user = this.usersService.getUserById(assignedTo);
+
     const task: Task = {
       id: (this.tasks.length + 1).toString(),
       title,
@@ -16,7 +24,9 @@ export class TasksService {
       status: TaskStatus.Pending,
       assignedTo,
     };
+
     this.tasks.push(task);
+    this.notificationService.sendEmail(user.email, 'Новая задача', `Вы назначены ответственным за задачу: "${title}"`);
 
     return task;
   }
@@ -27,7 +37,12 @@ export class TasksService {
       throw new NotFoundException(`Задача с ID ${id} не найдена`);
     }
 
+    const user = this.usersService.getUserById(task.assignedTo);
+
     Object.assign(task, updateTaskDto);
+
+    this.notificationService.sendSMS(user.phone, `Статус задачи "${task.title}" обновлён на "${updateTaskDto.status}"`);
+
     return task;
   }
 }
